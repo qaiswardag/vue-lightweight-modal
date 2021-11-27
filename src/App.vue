af79ae3c8a6fd60f5067fce992990cde995d15f1
qaiswardag <qais.wardag@outlook.com>
2021-11-27 16:44:32 +0100

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+
+  <div class="container">
+    <router-view />
+    <button class="btn btn--top-margin btn--center" @click="toggleModal">Modal</button>
+    <div v-if="showModal">
+      <Modal @close="toggleModal"></Modal>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+  },
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal" @click="closeModal">
+    <div class="modal-container">
+      <h3>consectetur adipisicing</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum et
+        officiis saepe.
+      </p>
+      <form action="">
+        <label>soluta totam</label>
+        <input type="text" placeholder="soluta totam">
+        <label>soluta totam</label>
+        <input type="text" placeholder="inventore">
+        <button class="btn btn--top-margin">submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
